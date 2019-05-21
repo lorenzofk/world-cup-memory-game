a26da4d460ef218a527a269ad773f0ac48e0414c
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2019-05-20 22:44:30 -0300

    Creating a component for MatchesList and to represent a single Match

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,29 +1,60 @@
 <template>
   <div class="game">
-    <h1> VueJS + Vuex </h1>
+    
+    <div v-if="loading" class="centered">
+      <img src="/assets/loader.gif">
+    </div>
+    
+    <div v-else class="container">
+      <h3> {{ currentStage }} </h3>
+      <hr>
+      <MatchesList />
+      <button @click="changeStage" class="btn btn-primary"> Next Stage </button>
+    </div>
+    
   </div>
 </template>
 
 <script>
 
+import MatchesList from "@/components/MatchesList";
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: "Game",
-  props: {},
+  components: { MatchesList },
   computed: {
-    ...mapGetters(['loading']),
+    ...mapGetters([
+      'loading', 
+      'matches', 
+      'currentStage'
+    ]),
   },
   created() {
-    this.fetchMatches();
+
+    if (! this.matches.length) {
+      this.fetchMatches();
+    }
+
   },
   methods: {
-    ...mapActions(['fetchMatches']),
+    ...mapActions([
+      'fetchMatches', 
+      'changeStage'
+    ]),
   },
 }
 
 </script>
 
 <style scoped>
-
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transform: -webkit-translate(-50%, -50%);
+  transform: -moz-translate(-50%, -50%);
+  transform: -ms-translate(-50%, -50%);
+}
 </style>
---
+++ src/components/MatchesList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="matches-list">
+
+    <div v-for="(match, index) in matches" :key="index">
+        <div class="row">
+          <div class="col-sm-5">
+            <a> 
+              <strong> {{ match.home_team.country }} </strong>
+            </a>
+          </div>
+          <div class="col-sm-2"> 
+            <strong> X </strong>
+          </div>
+          <div class="col-sm-5"> 
+            <a> 
+              <strong> {{ match.away_team.country }} </strong>
+            </a>
+          </div>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: "MatchesList",
+  props: {},
+  computed: {
+    ...mapGetters(['matches']),
+  },
+  methods: {
+  },
+}
+
+</script>
